22b32cfacd073bdd6807c8db64de9ee0ea47c3d2
asnav <naveenkishore.al22@bitsathy.ac.in>
2024-09-26 08:17:57 +0530

    Add operator homescreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -14,71 +14,53 @@ class HomeScreen extends StatefulWidget {
 class HomeScreenState extends State<HomeScreen> {
   final String serverKey = "";
   User? _user;
+  List<Map<String, dynamic>> _requests = [];
+  List<Map<String, dynamic>> _approvedLogs = [];
+  List<bool> _approvedStatus = [];
+  int _approvalCount = 0;
+  int _cancellationCount = 0;
 
-  // Variables to store dropdown selections
-  String? _selectedLocation;
-  String? _selectedDestination;
-  String? _selectedDesignation;
-  String? _selectedLuggageStatus;
-  String? _selectedPurpose;
-  String? _otherPurposeText;
 
-  final List<String> _locations = [
-    'Boys Hostel',
-    'Girls Hostel',
-    'Sunflower Block',
-    'Mechanical Block',
-    'As Block',
-    'IB Block',
-    'Cafeteria',
-    'BIT Main Auditorium',
-    'Aero Block',
-    'Gate A',
-    'Learning Centre',
-    'Medical Centre',
-    'Gate C',
-    'Badminton Court',
-    'Main Ground'
-  ];
 
   @override
   void initState() {
     super.initState();
     _user = FirebaseAuth.instance.currentUser;
-  }
 
-  Future<void> sendMessage() async {
-    const postUrl = 'https://fcm.googleapis.com/fcm/send';
-    final data = {
-      "to": "RECEIVER_DEVICE_TOKEN",
-      "notification": {
-        "title": "Message from App 1",
-        "body": "Hello from another app!",
-        "sound": "default",
+    _requests = [
+      {
+        'mailId': 'exa@mail.com',
+        'regNo': 'ABC1234',
+        'from': 'Campus A',
+        'to': 'Campus B',
+        'designation': 'Student',
+        'luggageStatus': 'Yes',
+        'specification': '',
       },
-      "data": {
-        "click_action": "FLUTTER_NOTIFICATION_CLICK",
-        "message": "This is a message from App 1"
-      }
-    };
+      {
+        'mailId': 'ple@mail.com',
+        'regNo': 'ABC1234',
+        'from': 'Campus A',
+        'to': 'Campus B',
+        'designation': 'Student',
+        'luggageStatus': 'Yes',
+        'specification': '',
+      },
+      {
+        'mailId': 'example@mail.com',
+        'regNo': 'ABC1234',
+        'from': 'Campus A',
+        'to': 'Campus B',
+        'designation': 'Student',
+        'luggageStatus': 'Yes',
+        'specification': '',
+      },
 
-    final headers = {
-      'content-type': 'application/json',
-      'Authorization': 'key=$serverKey',
-    };
-
-    final response = await http.post(
-      Uri.parse(postUrl),
-      body: json.encode(data),
-      headers: headers,
-    );
-
-    if (response.statusCode == 200) {
-      print('Message sent successfully');
-    } else {
-      print('Failed to send message');
-    }
+    ];
+    // Initialize the approval status list
+    _approvedStatus = List<bool>.filled(_requests.length, false);
   }
+
 
   Future<void> signUserOut() async {
     await FirebaseAuth.instance.signOut();
@@ -89,6 +71,89 @@ class HomeScreenState extends State<HomeScreen> {
       context,
       MaterialPageRoute(builder: (context) => const LoginScreen()),
       (Route<dynamic> route) => false,
+    );
+  }
+
+  void _showDetailsDialog(int index) {
+    Map<String, dynamic> request = _requests[index];
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('E-Vehicle Request Details'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: [
+                Text('Mail ID: ${request['mailId']}'),
+                Text('Reg No: ${request['regNo']}'),
+                Text('From: ${request['from']}'),
+                Text('To: ${request['to']}'),
+                Text('Designation: ${request['designation']}'),
+                Text('Luggage Status: ${request['luggageStatus']}'),
+                Text('Specification: ${request['specification']}'),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              child: const Text('Approve'),
+              onPressed: () {
+                setState(() {
+                  _approvedLogs.insert(0,request);
+                  _approvalCount++;
+                  _approvedStatus[index] = true; // Mark as approved
+                });
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () {
+                setState(() {
+                  _cancellationCount++;
+                  _approvedStatus[index] = true; // Mark as unapproved
+                });
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showEvLogs() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Ev-Logs'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('Total Approvals: $_approvalCount'),
+              Text('Total Cancellations: $_cancellationCount'),
+              const SizedBox(height: 10),
+              ..._approvedLogs.take(2).map((log) {
+                return ListTile(
+                  title: Text('Approved: ${log['mailId']}'),
+                  subtitle: Text('From: ${log['from']} To: ${log['to']}'),
+                );
+              }).toList(),
+              if (_approvedLogs.length > 2) // Show a message if there are more than two
+                Text('And ${_approvedLogs.length - 2} more...'),
+            ],
+          ),
+          actions: [
+            TextButton(
+              child: const Text('Close'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 
@@ -124,6 +189,14 @@ class HomeScreenState extends State<HomeScreen> {
               ),
             ),
             ListTile(
+              leading: const Icon(Icons.library_books),
+              title: const Text('Recent Approval'),
+              onTap: () {
+                Navigator.pop(context);
+                _showEvLogs();
+              },
+            ),
+            ListTile(
               leading: const Icon(Icons.logout),
               title: const Text('Logout'),
               onTap: () {
@@ -142,205 +215,75 @@ class HomeScreenState extends State<HomeScreen> {
             end: Alignment.bottomCenter,
           ),
         ),
-        child: Center(
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Card(
-                elevation: 10,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const Text(
-                        'Book Your Slot',
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.deepPurple,
-                        ),
-                      ),
-                      const SizedBox(height: 20),
-
-                      // Dropdown for Current Location
-                      DropdownButtonFormField<String>(
-                        value: _selectedLocation,
-                        decoration: InputDecoration(
-                          labelText: 'Current Location',
-                          filled: true,
-                          fillColor: Colors.white,
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                        ),
-                        items: _locations.map((String value) {
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value),
-                          );
-                        }).toList(),
-                        onChanged: (newValue) {
-                          setState(() {
-                            _selectedLocation = newValue;
-                            _selectedDestination = null;
-                          });
-                        },
-                      ),
-                      const SizedBox(height: 20),
-
-                      // Dropdown for Destination Location
-                      DropdownButtonFormField<String>(
-                        value: _selectedDestination,
-                        decoration: InputDecoration(
-                          labelText: 'Destination Location',
-                          filled: true,
-                          fillColor: Colors.white,
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                        ),
-                        items: _locations
-                            .where((location) => location != _selectedLocation)
-                            .map((String value) {
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value),
-                          );
-                        }).toList(),
-                        onChanged: (newValue) {
-                          setState(() {
-                            _selectedDestination = newValue;
-                          });
-                        },
-                      ),
-                      const SizedBox(height: 20),
-
-                      // Designation
-                      DropdownButtonFormField<String>(
-                        value: _selectedDesignation,
-                        decoration: InputDecoration(
-                          labelText: 'Designation',
-                          filled: true,
-                          fillColor: Colors.white,
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                        ),
-                        items: <String>['Student', 'Faculty', 'Technician']
-                            .map((String value) {
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value),
-                          );
-                        }).toList(),
-                        onChanged: (newValue) {
-                          setState(() {
-                            _selectedDesignation = newValue;
-                          });
-                        },
-                      ),
-                      const SizedBox(height: 20),
-
-                      // Luggage Status
-                      DropdownButtonFormField<String>(
-                        value: _selectedLuggageStatus,
-                        decoration: InputDecoration(
-                          labelText: 'Luggage Available Status',
-                          filled: true,
-                          fillColor: Colors.white,
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                        ),
-                        items: <String>['Yes', 'No'].map((String value) {
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value),
-                          );
-                        }).toList(),
-                        onChanged: (newValue) {
-                          setState(() {
-                            _selectedLuggageStatus = newValue;
-                          });
-                        },
-                      ),
-                      const SizedBox(height: 20),
-
-                      if (_selectedLuggageStatus == 'No') ...[
-                        // Purpose Dropdown
-                        DropdownButtonFormField<String>(
-                          value: _selectedPurpose,
-                          decoration: InputDecoration(
-                            labelText: 'Purpose',
-                            filled: true,
-                            fillColor: Colors.white,
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                          ),
-                          items: <String>['Meeting', '.', '.', 'Other']
-                              .map((String value) {
-                            return DropdownMenuItem<String>(
-                              value: value,
-                              child: Text(value),
-                            );
-                          }).toList(),
-                          onChanged: (newValue) {
-                            setState(() {
-                              _selectedPurpose = newValue;
-                              _otherPurposeText = null;
-                            });
-                          },
-                        ),
-                        const SizedBox(height: 20),
-
-                        // Other Purpose Text Field
-                        if (_selectedPurpose == 'Other') ...[
-                          TextFormField(
-                            decoration: InputDecoration(
-                              labelText: 'Specify',
-                              filled: true,
-                              fillColor: Colors.white,
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                            ),
-                            onChanged: (text) {
-                              setState(() {
-                                _otherPurposeText = text;
-                              });
-                            },
-                          ),
-                          const SizedBox(height: 20),
-                        ],
-                      ],
-
-                      // Submit Button with Ripple Effect
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 40, vertical: 15),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30),
-                          ),
-                          backgroundColor: Colors.deepPurple,
-                          foregroundColor: Colors.white, // Correct parameter
-                        ),
-                        onPressed: () {
-                          // Handle form submission here
-                        },
-                        child: const Text('Submit'),
-                      ),
-                    ],
+        child: Column(
+          children: <Widget>[
+            // Top bar with rounded corners
+            Container(
+              margin: const EdgeInsets.all(16.0), // Margin around the bar
+              padding: const EdgeInsets.all(16.0), // Padding inside the bar
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20.0), // Rounded corners
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 8.0,
+                    offset: Offset(0, 4), // Shadow position
                   ),
-                ),
+                ],
+              ),
+              child: const Column(
+                children: [
+                  Text(
+                    'REQUEST APPROVE',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ],
               ),
             ),
-          ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _requests.length,
+                itemBuilder: (context, index) {
+                  if (_approvedStatus[index]) {
+                    return SizedBox.shrink(); // Skip approved requests
+                  }
+                  return Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                    padding: const EdgeInsets.all(16.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black26,
+                          blurRadius: 5.0,
+                          offset: Offset(0, 2),
+                        ),
+                      ],
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded( // Use Expanded here
+                          child: Text(
+                            '${index + 1}. Request from: ${_requests[index]['mailId']}',
+                            style: TextStyle(fontSize: 18.0),
+                            overflow: TextOverflow.ellipsis, // Prevent overflow
+                            maxLines: 1, // Limit to one line
+                          ),
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.visibility_sharp),
+                          onPressed: () => _showDetailsDialog(index),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
