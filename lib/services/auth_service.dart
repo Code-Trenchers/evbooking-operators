23fed51bf5f9b999589362160d71be55e9e29be2
JEEVITHA KANNAN K S <ksjeevithakannan123@gmail.com>
2024-10-24 10:01:16 +0530

    refact: db and auth service code (#7)

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,8 +1,34 @@
-import 'package:evbooking_operators/services/logger_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:evbooking_operators/services/logger_service.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  Future<User?> gmailLogin() async {
+    LoggerService.info('Attempting Gmail login');
+
+    try {
+      await _googleSignIn.signOut();
+      final googleUser = await _googleSignIn.signIn();
+      if (googleUser == null) {
+        LoggerService.info('Google sign-in cancelled by user');
+        return null;
+      }
+
+      final googleAuth = await googleUser.authentication;
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      return await signInWithCredential(credential);
+    } on FirebaseAuthException catch (e) {
+      LoggerService.error('Firebase Auth Exception during Gmail login', e);
+      return null;
+    }
+  }
 
   Future<User?> signIn(String email, String password) async {
     try {
@@ -33,6 +59,7 @@ class AuthService {
     try {
       LoggerService.info('Attempting to sign out user');
       await _auth.signOut();
+      await _googleSignIn.signOut();
       LoggerService.info('User signed out successfully');
     } catch (e) {
       LoggerService.error('Error signing out', e);
